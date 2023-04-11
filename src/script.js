090287d9d714ba70ae963613f79925688ddd90a6
MarcusDantas <marvinsd@gmail.com>
2023-04-11 15:01:45 -0300

    feat: add game logic

--- src/script.js
+++ src/script.js
@@ -1,12 +1,17 @@
-const cards = document.querySelectorAll('.card-number');
+let pointsNeeded, turns, points;
+let firstCard = null, secondCard = null;
+
+const numberCards = document.querySelectorAll('.card-number');
+numberCards.forEach(card => card.addEventListener('click', startGame));
 
 function startGame() {
+    turns = points = 0;
+
     this.classList.toggle('flip-number');
     const numberPairs = parseInt(this.innerHTML)/2;
+    pointsNeeded = numberPairs;
     fetchCards(numberPairs);
 }
-
-cards.forEach(card => card.addEventListener('click', startGame));
 
 async function fetchCards(numberPairs) {
     try {
@@ -36,22 +41,64 @@ function randomShuffle() {
 
 function renderCards(listCards) {
     const cardImages = listCards.map(card => card.image)
-    console.log(cardImages);
     const cardsContainer = document.querySelector(".cards-container");
     cardsContainer.innerHTML='';
 
     for (let i = 0; i < cardImages.length; i++) {
         cardsContainer.innerHTML += `
-            <li class="card" onclick="flipCard(this)">
+            <li class="card">
                 <img class="front-face" src=${cardImages[i]} alt="front of the card">
                 <img class="back-face" src="./src/img/back.png" alt="back of the card">
             </li>
             `;
     }
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => card.addEventListener('click', manageGame));
 }
 
 function flipCard(card) {
     card.classList.toggle('flip');
 }
 
+function manageGame() {  
+    if (firstCard !== null && secondCard !== null) {
+        return;
+    }
+    else if (firstCard === null) {
+        flipCard(this);
+        firstCard = this;
+        return;
+    } 
+    else if (firstCard === this) {
+        return;
+    }
   
+    flipCard(this);
+    secondCard = this;
+    turns++;
+    setTimeout(compareCards, 1000);
+}
+
+function compareCards() {
+    if (firstCard.innerHTML === secondCard.innerHTML) {
+        firstCard = null;
+        secondCard = null;
+        managePoints();
+        return;
+    }
+  
+    flipCard(firstCard);
+    flipCard(secondCard);
+    firstCard = null;
+    secondCard = null;
+}
+
+function managePoints() {
+    points++;
+
+    if (points === pointsNeeded) {
+      alert(
+        `Você ganhou em ${turns} jogadas!`
+      );
+    }
+}
